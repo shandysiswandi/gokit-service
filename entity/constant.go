bb85346f14a1329e46921dfd2a99b1e25f45033b
Shandy Siswandi <shandysiswandi@gmail.com>
2021-09-07 23:43:00 +0700

    update constant

--- entity/constant.go
+++ entity/constant.go
@@ -1,8 +1,12 @@
 package entity
 
-type Status string
+// standart field in response [optional]
+//
+// code 	int32 		example: 200, 400, 500
+// status	string		example: "OK", "Bad Request", "Internal Server Error"
+// message 	string		example: "Success", "Something wrong with request", "Something wrong with server"
+// data		interface{}	example: [], {}, nil
 
 const (
-	DONE  Status = "done"
-	DRAFT Status = "draft"
+	AppName = "gokit-service"
 )
