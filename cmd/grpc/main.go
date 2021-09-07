6715696ea82e4ab874fc5f42458cde651004a6c3
Shandy Siswandi <shandysiswandi@gmail.com>
2021-09-07 23:46:03 +0700

    create grpc entrypoint

---
+++ cmd/grpc/main.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	// setup environment variable
+
+	// setup timezone
+
+	// setup logger
+
+	// setup repository databse
+
+	// setup repository cache
+
+	// setup business logic
+
+	// setup endpoints (controller)
+
+	// setup tranport (gRPC server)
+}
