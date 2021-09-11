88fb4e283b419842cc6f4362f6bd92aebf431190
Shandy Siswandi <shandysiswandi@gmail.com>
2021-09-12 00:45:35 +0700

    refactor cmd/http/main.go to be nicely

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -8,11 +8,14 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
 	"github.com/joho/godotenv"
 	"github.com/shandysiswandi/gokit-service/endpoint"
 	"github.com/shandysiswandi/gokit-service/pkg/env"
-	"github.com/shandysiswandi/gokit-service/pkg/logger"
+	"github.com/shandysiswandi/gokit-service/repository"
 	"github.com/shandysiswandi/gokit-service/repository/postgres"
+	"github.com/shandysiswandi/gokit-service/repository/redis"
 	"github.com/shandysiswandi/gokit-service/service"
 	"github.com/shandysiswandi/gokit-service/transport/httptrans"
 )
@@ -20,7 +23,7 @@ import (
 func main() {
 	// setup environment variable
 	if err := godotenv.Load(".env"); err != nil {
-		logger.Info("godotenv.Load " + err.Error())
+		println(err.Error())
 		os.Exit(1)
 	}
 
@@ -28,33 +31,63 @@ func main() {
 	os.Setenv("TZ", env.Get("APP_TZ"))
 
 	// setup logger
-	logger.SetOutput(os.Stderr)
+	var logger log.Logger
+	{
+		logger = log.NewJSONLogger(log.NewSyncWriter(os.Stderr))
+		timezone := log.DefaultTimestampUTC
+		if env.Get("APP_TZ") != "UTC" {
+			timezone = log.DefaultTimestamp
+		}
+		logger = log.With(logger, "ts", timezone)
+		logger = log.With(logger, "caller", log.DefaultCaller)
+	}
 
 	// setup repository databse
-	dbRW, err := postgres.NewPostgres(postgres.Configuration{
-		Host:     env.Get("DB_HOST"),
-		Port:     env.GetInt("DB_PORT"),
-		Username: env.Get("DB_USERNAME"),
-		Password: env.Get("DB_PASSWORD"),
-		Database: env.Get("DB_DATABASE"),
-		Options:  env.Get("DB_OPTIONS"),
-	})
-	if err != nil {
-		logger.Info("postgres.NewPostgres " + err.Error())
-		os.Exit(1)
+	var dbRW repository.DatabaseReaderWriter
+	var err error
+	{
+		dbRW, err = postgres.NewPostgres(postgres.Configuration{
+			Host:     env.Get("DB_HOST"),
+			Port:     env.GetInt("DB_PORT"),
+			Username: env.Get("DB_USERNAME"),
+			Password: env.Get("DB_PASSWORD"),
+			Database: env.Get("DB_DATABASE"),
+			Options:  env.Get("DB_OPTIONS"),
+		})
+		if err != nil {
+			level.Error(logger).Log("msg", err)
+			os.Exit(1)
+		}
 	}
 
 	// setup repository cache
-	// var cacheRW repository.CacheReaderWriter
+	var cacheRW repository.CacheReaderWriter
+	{
+		cacheRW, err = redis.NewRedis()
+		if err != nil {
+			level.Error(logger).Log("msg", err)
+			os.Exit(1)
+		}
+	}
 
 	// setup business logic
-	srv := service.NewTodoService(dbRW, nil)
+	var srv service.TodoService
+	{
+		srv = service.NewTodoService(logger, dbRW, cacheRW)
+		srv = service.NewMiddleware(logger)(srv)
+	}
 
 	// setup endpoints (controller)
-	end := endpoint.NewEndpoints(srv)
+	var end endpoint.Endpoints
+	{
+		end = endpoint.NewEndpoints(srv, env.Get("JWT_SECRET"))
+	}
 
 	// setup tranport (router)
-	h := httptrans.NewServer(end, env.Get("JWT_SECRET"))
+	var h http.Handler
+	{
+		h = httptrans.NewServer(end)
+	}
 
 	// setup http server
 	server := &http.Server{
@@ -68,11 +101,11 @@ func main() {
 	// running server in groutine
 	go func() {
 		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			logger.Info("server.ListenAndServe " + err.Error())
+			level.Error(logger).Log("msg", err)
 			os.Exit(1)
 		}
 	}()
-	logger.Info("server listen on port " + env.Get("APP_PORT"))
+	level.Info(logger).Log("msg", "server listen on port "+env.Get("APP_PORT"))
 
 	// setup channel for listen some trigger
 	c := make(chan os.Signal, 1)
@@ -90,9 +123,9 @@ func main() {
 
 	// shutdown server
 	if err := server.Shutdown(ctx); err != nil {
-		logger.Info("server.Shutdown " + err.Error())
+		level.Error(logger).Log("msg", err)
 		os.Exit(1)
 	}
 
-	logger.Info("shutting down server")
+	level.Info(logger).Log("msg", "shutting down server ")
 }
