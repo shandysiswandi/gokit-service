11d2b67a24dcca6476b0e09271cb90b9d679473d
Shandy Siswandi <shandysiswandi@gmail.com>
2021-09-12 14:54:27 +0700

    implement redis logic and test for GetAllTodo

--- repository/redis/get_all_todo.go
+++ repository/redis/get_all_todo.go
@@ -1,11 +1,34 @@
 package redis
 
-import "github.com/shandysiswandi/gokit-service/entity"
+import (
+	"context"
+	"encoding/json"
+	"time"
 
-func (r *redisCache) GetAllTodo() []entity.Todo {
-	return nil
+	"github.com/shandysiswandi/gokit-service/entity"
+)
+
+func (r *redisCache) GetAllTodo(ctx context.Context, k string) []entity.Todo {
+	data := make([]entity.Todo, 0)
+	r.mu.RLock()
+	value, err := r.client.Get(ctx, k).Result()
+	r.mu.RUnlock()
+	if err != nil {
+		return data
+	}
+
+	err = json.Unmarshal([]byte(value), &data)
+	if err != nil {
+		return data
+	}
+
+	return data
 }
 
-func (r *redisCache) SetAllTodo() error {
-	return nil
+func (r *redisCache) SetAllTodo(ctx context.Context, k string, v []entity.Todo) error {
+	bytes, _ := json.Marshal(v)
+	r.mu.Lock()
+	err := r.client.Set(ctx, k, bytes, 10*time.Second).Err()
+	r.mu.Unlock()
+	return err
 }
--- repository/redis/get_all_todo_test.go
+++ repository/redis/get_all_todo_test.go
@@ -1,11 +1,105 @@
 package redis
 
-import "testing"
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"testing"
+	"time"
+
+	"github.com/go-redis/redismock/v8"
+	"github.com/shandysiswandi/gokit-service/entity"
+	"github.com/stretchr/testify/assert"
+)
 
 func Test_redisCache_GetAllTodo(t *testing.T) {
-	// TODO:
+	db, mock := redismock.NewClientMock()
+	defer db.Close()
+
+	type args struct {
+		ctx context.Context
+		key string
+	}
+	testTable := []struct {
+		title   string
+		args    args
+		mocking func(a args)
+		want    []entity.Todo
+	}{
+		{
+			title:   "Negative r.client.Get",
+			args:    args{ctx: context.Background(), key: "some"},
+			mocking: func(a args) {},
+			want:    []entity.Todo{},
+		},
+		{
+			title: "Negative json.Unmarshal",
+			args:  args{ctx: context.Background(), key: "some"},
+			mocking: func(a args) {
+				mock.ExpectGet(a.key).SetVal("error")
+			},
+			want: []entity.Todo{},
+		},
+		{
+			title: "Positive",
+			args:  args{ctx: context.Background(), key: "some"},
+			mocking: func(a args) {
+				val, _ := json.Marshal([]entity.Todo{})
+				mock.ExpectGet(a.key).SetVal(string(val))
+			},
+			want: []entity.Todo{},
+		},
+	}
+
+	for _, tc := range testTable {
+		t.Run(tc.title, func(t *testing.T) {
+			tc.mocking(tc.args)
+			rdb := redisCache{client: db}
+			data := rdb.GetAllTodo(tc.args.ctx, tc.args.key)
+			assert.Equal(t, tc.want, data)
+		})
+	}
 }
 
 func Test_redisCache_SetAllTodo(t *testing.T) {
-	// TODO:
+	db, mock := redismock.NewClientMock()
+	defer db.Close()
+
+	type args struct {
+		ctx   context.Context
+		key   string
+		value []entity.Todo
+	}
+	testTable := []struct {
+		title   string
+		args    args
+		mocking func(a args)
+		wantErr bool
+	}{
+		{
+			title:   "Negative r.client.Get",
+			args:    args{ctx: context.Background(), key: "some"},
+			mocking: func(a args) {},
+			wantErr: true,
+		},
+		{
+			title: "Positive",
+			args:  args{ctx: context.Background(), key: "some", value: make([]entity.Todo, 0)},
+			mocking: func(a args) {
+				val, _ := json.Marshal(a.value)
+				mock.ExpectSet(a.key, val, time.Second*10).SetVal(string(val))
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, tc := range testTable {
+		t.Run(tc.title, func(t *testing.T) {
+			tc.mocking(tc.args)
+			rdb := redisCache{client: db}
+			err := rdb.SetAllTodo(tc.args.ctx, tc.args.key, tc.args.value)
+			log.Println(err)
+			assert.Equal(t, tc.wantErr, err != nil)
+		})
+	}
 }
