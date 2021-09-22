6abe54ec418e3c3f3eeb1117ef5e129b01b4cc11
Shandy Siswandi <shandysiswandi@gmail.com>
2021-09-22 21:36:15 +0700

    update endpoint

--- endpoint/endpoint.go
+++ endpoint/endpoint.go
@@ -5,115 +5,106 @@ import (
 	"errors"
 	"net/http"
 
-	"github.com/go-kit/kit/endpoint"
 	"github.com/shandysiswandi/gokit-service/entity"
 	"github.com/shandysiswandi/gokit-service/pkg/uuid"
 	"github.com/shandysiswandi/gokit-service/service"
 )
 
-type Endpoints struct {
-	GetAllTodo  endpoint.Endpoint
-	GetTodoByID endpoint.Endpoint
-	CreateTodo  endpoint.Endpoint
-	UpdateTodo  endpoint.Endpoint
-	DeleteTodo  endpoint.Endpoint
+type Endpoints interface {
+	GetAllTodo(ctx context.Context, request interface{}) (response interface{}, err error)
+	GetTodoByID(ctx context.Context, request interface{}) (response interface{}, err error)
+	CreateTodo(ctx context.Context, request interface{}) (response interface{}, err error)
+	UpdateTodo(ctx context.Context, request interface{}) (response interface{}, err error)
+	DeleteTodo(ctx context.Context, request interface{}) (response interface{}, err error)
+}
+
+type endpoints struct {
+	jwtSecret string
+	service   service.TodoService
 }
 
 func NewEndpoints(srv service.TodoService, secret string) Endpoints {
-	return Endpoints{
-		GetAllTodo:  JWTMiddleware(secret)(makeGetAllTodo(srv)),
-		GetTodoByID: JWTMiddleware(secret)(makeGetTodoByID(srv)),
-		CreateTodo:  JWTMiddleware(secret)(makeCreateTodo(srv)),
-		UpdateTodo:  JWTMiddleware(secret)(makeUpdateTodo(srv)),
-		DeleteTodo:  JWTMiddleware(secret)(makeDeleteTodo(srv)),
+	return &endpoints{
+		jwtSecret: secret,
+		service:   srv,
 	}
 }
 
-func makeGetAllTodo(s service.TodoService) endpoint.Endpoint {
-	return func(ctx context.Context, request interface{}) (interface{}, error) {
-		req := request.(entity.GetAllTodoTodoRequest)
+func (e *endpoints) GetAllTodo(ctx context.Context, request interface{}) (interface{}, error) {
+	req := request.(entity.GetAllTodoRequest)
 
-		data, err := s.GetAllTodo(ctx, req)
-		if err != nil {
-			return nil, err
-		}
-
-		return entity.GetAllTodoTodoResponse{
-			Code:    http.StatusOK,
-			Status:  http.StatusText(http.StatusOK),
-			Message: "Success get all todo",
-			Data:    data,
-		}, nil
+	data, err := e.service.GetAllTodo(ctx, req)
+	if err != nil {
+		return nil, err
 	}
+
+	return entity.GetAllTodoResponse{
+		Code:    http.StatusOK,
+		Status:  http.StatusText(http.StatusOK),
+		Message: "Success get all todo",
+		Data:    data,
+	}, nil
 }
 
-func makeGetTodoByID(s service.TodoService) endpoint.Endpoint {
-	return func(ctx context.Context, request interface{}) (interface{}, error) {
-		req := request.(entity.GetTodoByIDTodoRequest)
+func (e *endpoints) GetTodoByID(ctx context.Context, request interface{}) (interface{}, error) {
+	req := request.(entity.GetTodoByIDRequest)
 
-		// validate req
-		if ok := uuid.IsValidUUID(req.ID); !ok {
-			return nil, errors.New("your request id is malformat")
-		}
-
-		data, err := s.GetTodoByID(ctx, req)
-		if err != nil {
-			return nil, err
-		}
-
-		return entity.GetTodoByIDTodoResponse{
-			Code:    http.StatusOK,
-			Status:  http.StatusText(http.StatusOK),
-			Message: "Success get todo by id",
-			Data:    data,
-		}, nil
+	// validate req
+	if ok := uuid.IsValidUUID(req.ID); !ok {
+		return nil, errors.New("your request id is malformat")
 	}
+
+	data, err := e.service.GetTodoByID(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	return entity.GetTodoByIDResponse{
+		Code:    http.StatusOK,
+		Status:  http.StatusText(http.StatusOK),
+		Message: "Success get todo by id",
+		Data:    data,
+	}, nil
 }
 
-func makeCreateTodo(s service.TodoService) endpoint.Endpoint {
-	return func(ctx context.Context, request interface{}) (interface{}, error) {
-		req := request.(entity.CreateTodoRequest)
+func (e *endpoints) CreateTodo(ctx context.Context, request interface{}) (interface{}, error) {
+	req := request.(entity.CreateTodoRequest)
 
-		if err := s.CreateTodo(ctx, req); err != nil {
-			return nil, err
-		}
-
-		return entity.CreateTodoResponse{
-			Code:    http.StatusOK,
-			Status:  http.StatusText(http.StatusOK),
-			Message: "Success create todo",
-		}, nil
+	if err := e.service.CreateTodo(ctx, req); err != nil {
+		return nil, err
 	}
+
+	return entity.CreateTodoResponse{
+		Code:    http.StatusOK,
+		Status:  http.StatusText(http.StatusOK),
+		Message: "Success create todo",
+	}, nil
 }
 
-func makeUpdateTodo(s service.TodoService) endpoint.Endpoint {
-	return func(ctx context.Context, request interface{}) (interface{}, error) {
-		req := request.(entity.UpdateTodoRequest)
+func (e *endpoints) UpdateTodo(ctx context.Context, request interface{}) (interface{}, error) {
+	req := request.(entity.UpdateTodoRequest)
 
-		if err := s.UpdateTodo(ctx, req); err != nil {
-			return nil, err
-		}
-
-		return entity.UpdateTodoResponse{
-			Code:    http.StatusOK,
-			Status:  http.StatusText(http.StatusOK),
-			Message: "Success update todo",
-		}, nil
+	if err := e.service.UpdateTodo(ctx, req); err != nil {
+		return nil, err
 	}
+
+	return entity.UpdateTodoResponse{
+		Code:    http.StatusOK,
+		Status:  http.StatusText(http.StatusOK),
+		Message: "Success update todo",
+	}, nil
 }
 
-func makeDeleteTodo(s service.TodoService) endpoint.Endpoint {
-	return func(ctx context.Context, request interface{}) (interface{}, error) {
-		req := request.(entity.DeleteTodoRequest)
+func (e *endpoints) DeleteTodo(ctx context.Context, request interface{}) (interface{}, error) {
+	req := request.(entity.DeleteTodoRequest)
 
-		if err := s.DeleteTodo(ctx, req); err != nil {
-			return nil, err
-		}
-
-		return entity.DeleteTodoResponse{
-			Code:    http.StatusOK,
-			Status:  http.StatusText(http.StatusOK),
-			Message: "Success delete todo",
-		}, nil
+	if err := e.service.DeleteTodo(ctx, req); err != nil {
+		return nil, err
 	}
+
+	return entity.DeleteTodoResponse{
+		Code:    http.StatusOK,
+		Status:  http.StatusText(http.StatusOK),
+		Message: "Success delete todo",
+	}, nil
 }
